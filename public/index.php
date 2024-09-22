42939b660e71c4d421644cd562d4f876a6b3179c
Gabriel Enrique Vargas Perez <98557334+DarkLouses@users.noreply.github.com>
2024-09-22 21:40:00 +0200

    fix(request): initialize route property in constructor
    Ensure the route property is properly initialized in the Request class constructor to avoid accessing it before initialization. This fixes the Typed property error.

--- public/index.php
+++ public/index.php
@@ -2,37 +2,22 @@
 
 require_once '../vendor/autoload.php';
 
-use Knight\Http\HttpNotFoundException;
+use Knight\App;
 use Knight\Http\Request;
 use Knight\Http\Response;
-use Knight\Routing\Router;
-use Knight\Server\PhpNativeServer;
 
-$router = new Router();
+$app = App::bootstrap();
 
-$router->get('/test/{param}', function (Request $request) {
+$app->router->get('/test/{param}', function (Request $request) {
 	return Response::json($request->routerParameters());
 });
 
-$router->post('/test', function (Request $request) {
+$app->router->post('/test', function (Request $request) {
 	return Response::json($request->data());
 });
 
-$router->get('/redirect', function (Request $request) {
+$app->router->get('/redirect', function (Request $request) {
 	return Response::redirect('/test');
 });
 
-$server = new PhpNativeServer();
-
-try {
-	$request = $server->getRequest();
-    $route = $router->resolve($request);
-	$request->setRoute($route);
-    $action = $route->action();
-	$response = $action($request);
-
-	$server->sendResponse($response);
-} catch (HttpNotFoundException $e) {
-	$response = Response::text('Not found')->setStatus(404);
-	$server->sendResponse($response);
-}
+$app->run();
---
+++ src/Knight/App.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Knight;
+
+use Knight\Container\Container;
+use Knight\Http\HttpNotFoundException;
+use Knight\Http\Request;
+use Knight\Http\Response;
+use Knight\Routing\Router;
+use Knight\Server\PhpNativeServer;
+use Knight\Server\Server;
+
+class App
+{
+	public Router $router;
+	public Server $server;
+	public Request $request;
+
+	public static function bootstrap(): mixed
+	{
+		$app = Container::singleton(App::class);
+		$app->router = new Router();
+		$app->server = new PhpNativeServer();
+		$app->request = $app->server->getRequest();
+
+		return $app;
+	}
+
+		public function run(): void
+		{
+			try {
+				$route = $this->router->resolve($this->request);
+				$this->request->setRoute($route);
+				$action = $route->action();
+				$response = $action($this->request);
+
+				$this->server->sendResponse($response);
+			} catch (HttpNotFoundException $e) {
+				$response = Response::text('Not found')->setStatus(404);
+				$this->server->sendResponse($response);
+			}
+		}
+}
